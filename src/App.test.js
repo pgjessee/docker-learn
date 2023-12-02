c49bc2f5adf1a030975b1a07ebb4031792a86ab8
pgjessee <peter.jessee@gmail.com>
2023-12-02 11:21:29 -0600

    testing CI

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,7 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/Hoof Hearted/i);
+  const pTag = screen.getByText('LOL!')
   expect(linkElement).toBeInTheDocument();
+  expect(pTag).toBeInTheDocument();
 });
